46e9027ec44063f3883aa4d0e3c6711aa5cc455d
sagarppatel <personal.sagarpatel@gmail.com>
2020-02-21 20:11:16 -0800

    small change

--- src/main/java/com/sachi1508/atn/project1/AtnProject1.java
+++ src/main/java/com/sachi1508/atn/project1/AtnProject1.java
@@ -3,6 +3,6 @@ package com.sachi1508.atn.project1;
 public class AtnProject1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello ATN!");
+        System.out.println("Hello World by ATN!");
     }
 }
